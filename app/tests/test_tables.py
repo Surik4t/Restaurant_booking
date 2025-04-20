b8a3e994b2bc56f2ca6d6326ae99de87b4ff389b
Surik4t <surik4t@yandex.ru>
2025-04-20 12:20:53 +0700

    add test_tables

---
+++ app/tests/test_tables.py
@@ -0,0 +1,12 @@
+import requests
+from app.routers.tables import add_table, delete_table
+
+def test_add_table_endpoint_test():
+    payload = {
+        "name": "test_table",
+        "seats": 2,
+        "location": "Somewhere over the rainbow"
+    }
+
+    response = add_table(payload)
+    assert response.status_code == 200
