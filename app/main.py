9a8a4bfed3fd7822029bd4cf11c295818f78f04d
Surik4t <surik4t@yandex.ru>
2025-04-22 10:40:31 +0700

    chores

--- app/main.py
+++ app/main.py
@@ -12,6 +12,3 @@ app.include_router(reservations.router)
 async def root():
     link = "<a href='http://127.0.0.1:8000/docs#/' style='font-size: 50'>API endpoints</a>"
     return HTMLResponse(content=link)
-
-
-print(app.routes)
